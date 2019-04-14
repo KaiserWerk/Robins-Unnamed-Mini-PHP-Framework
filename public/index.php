1f1082cc2c4cd6818a2ad4c5f394a1cb7543bc3c
Robin Kaiser <m@r-k.mx>
2019-04-14 20:43:04 +0200

    moved constants.php

--- public/index.php
+++ public/index.php
@@ -3,7 +3,8 @@
 // declaring strict types
 declare(strict_types=1);
 
-// include the constants
-include __DIR__ . '/../constants.php';
+// include the constants and base function
+include __DIR__ . '/../app/constants.php';
+include __DIR__ . '/../app/functions.php';
 // include the bootstrapping file
 include __DIR__ . '/../bootstrap.php';
