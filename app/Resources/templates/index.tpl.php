0497af0ce20016f409e7b2ddeaf9667519d1bcec
Robin Kaiser <m@r-k.mx>
2019-08-28 13:18:53 +0200

    Update index.tpl.php

--- app/Resources/templates/index.tpl.php
+++ app/Resources/templates/index.tpl.php
@@ -1,6 +1,6 @@
 <h1><?=$message;?></h1>
 <p>
-	Now, you can start working at your project!<br><br>
+	Now, you can start working on your project!<br>
 	Find the DefaultController.php in /app/Controller and the template at /app/Resources/templates.
 	<br><br>
 	Have fun!
